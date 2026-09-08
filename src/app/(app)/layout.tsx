4b4382ea3e9afb99889aa38c068a84b60f83cde6
lbsudo <lbsudo100@gmail.com>
2026-09-08 16:32:27 -0700

    chore(seo): block crawlers from cms.lbdluxe.com
    - robots.txt: Disallow /
    - noindex/noarchive metadata on the (app) layout

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -5,6 +5,12 @@ import '../globals.css'
 export const metadata = {
   description: 'Lbdluxe content management system',
   title: 'Lbdluxe',
+  robots: {
+    index: false,
+    follow: false,
+    nocache: true,
+    noarchive: true,
+  },
 }
 
 // eslint-disable-next-line no-restricted-exports
